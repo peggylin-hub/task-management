146f9c851d8557740ee790b26f9d325cb87bdfb9
Lin, Peggy <peggy.lin.syd@gmail.com>
2025-08-09 14:52:16 +1000

    add pin function
    add pin function

--- src/AppShell.xaml.cs
+++ src/AppShell.xaml.cs
@@ -5,6 +5,7 @@ namespace TaskManagementApp;
 public partial class AppShell : Shell
 {
 	public ObservableCollection<TodoItem> FlyoutTodos { get; set; } = new();
+	private bool _isPinned = true;
 
 	public AppShell()
 	{
@@ -16,6 +17,7 @@ public partial class AppShell : Shell
 		FlyoutTodos.Add(new TodoItem { Title = "Fix bug #123", IsCompleted = false });
 		
 		FlyoutTodoList.ItemsSource = FlyoutTodos;
+		UpdatePinButtonText();
 	}
 
 	private async void OnAddFlyoutTodoClicked(object sender, EventArgs e)
@@ -30,7 +32,45 @@ public partial class AppShell : Shell
 	private async void OnHomeClicked(object sender, EventArgs e)
 	{
 		await GoToAsync("//MainPage");
-		FlyoutIsPresented = false;
+		if (!_isPinned)
+			FlyoutIsPresented = false;
+	}
+
+	private void OnPinClicked(object sender, EventArgs e)
+	{
+		_isPinned = !_isPinned;
+		
+		if (_isPinned)
+		{
+			FlyoutBehavior = FlyoutBehavior.Locked;
+		}
+		else
+		{
+			FlyoutBehavior = FlyoutBehavior.Flyout;
+		}
+		
+		UpdatePinButtonText();
+	}
+
+	private void OnHideClicked(object sender, EventArgs e)
+	{
+		if (_isPinned)
+		{
+			// Temporarily unpin to allow hiding
+			FlyoutBehavior = FlyoutBehavior.Flyout;
+			FlyoutIsPresented = false;
+			// Set back to locked after hiding
+			FlyoutBehavior = FlyoutBehavior.Locked;
+		}
+		else
+		{
+			FlyoutIsPresented = false;
+		}
+	}
+
+	private void UpdatePinButtonText()
+	{
+		PinButton.Text = _isPinned ? "📍" : "📌";
 	}
 }
 
